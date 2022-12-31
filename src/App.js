d5c0fd496cb7697480ecea1be3da41db055729b4
Ngoc <ngoc43552@gmail.com>
2022-12-31 16:42:00 +0700

    update base project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import { Fragment } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { publicRoutes } from '~/routes'
 import { DefaultLayout } from "./components/Layout";
+import Newfeed from "~/pages/Newfeed";
+import Profile from "~/pages/Profile";
+import Login from "~/pages/Login";
+import Register from "~/pages/Register"; 
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          {publicRoutes.map((route, index) => {
+          <Route path="/" element={<DefaultLayout> <Newfeed /> </DefaultLayout>} />
+          <Route path="/profile" element={<DefaultLayout> <Profile /> </DefaultLayout>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+
+          {/* {publicRoutes.map((route, index) => {
             const Layout = route.layout === null ? Fragment : DefaultLayout;
             const Page = route.component;
             return <Route key={index} path = {route.path} element = {<Layout><Page /></Layout>}/>
           })}
-          
+           */}
         </Routes>
       </div>
     </Router>
--- src/components/Layout/DefaultLayout/Header.js
+++ src/components/Layout/DefaultLayout/Header.js
@@ -1,7 +1,3 @@
-import classNames from 'classnames/bind';
-import styles from './Header.module.css';
-
-const cx = classNames.bind(styles);
 
 function Header() {
     return (
