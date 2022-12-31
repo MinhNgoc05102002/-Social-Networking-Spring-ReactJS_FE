e874ee96566d98c6a0dfa74d0a967fb1c9093851
Ngoc <ngoc43552@gmail.com>
2022-12-31 16:43:41 +0700

    deleted some empty file

--- src/components/GlobalStyles/index.js
+++ src/components/GlobalStyles/index.js
@@ -1,10 +1,3 @@
-import './GlobalStyles.css'
-// import './css/main.min.css'
-// import './css/weather-icons.min.css'
-// import './css/nprogress.css'
-// import './css/style.css'
-// import './css/color.css'
-// import './css/responsive.css'
 
 function GlobalStyles({ children }) {
     return (children)
